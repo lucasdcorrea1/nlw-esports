036fff07093d29ff89e3e22e09c166fd8068f1e4
Lucas Corrêa <lucas.correa@multilaser.com.br>
2022-09-16 01:45:55 -0300

    first commit

---
+++ Server/src/server.ts
@@ -0,0 +1,22 @@
+import  express from 'express';
+
+const app = express();
+
+app.get('/v1/ads', (request, response) => {
+    return response.json([
+        {
+            id: 1,
+            name: "Lucas Damas Correa"
+        },
+        {
+            id: 2,
+            name: "Késsia Rodrigues Borges"
+        },
+        {
+            id: 3,
+            name: "Test 3"
+        }
+    ])
+});
+
+app.listen(8080);
